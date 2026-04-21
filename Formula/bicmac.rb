484328a715c9ebfd7bcb6cfa367a29f5ea5f1951
supaicy <iwilldtw@gmail.com>
2026-04-21 11:30:41 +0900

    bicmac 0.1.1

--- Formula/bicmac.rb
+++ Formula/bicmac.rb
@@ -2,7 +2,7 @@ class Bicmac < Formula
   desc "macOS menu bar utility: clipboard history, window snapping, app switching"
   homepage "https://github.com/supaicy/BicMac"
   url "https://github.com/supaicy/BicMac/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on :macos => :sonoma
