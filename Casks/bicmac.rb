d669b90b03650dcb1fb591966768227a041dfd8b
supaicy <iwilldtw@gmail.com>
2026-04-26 15:56:03 +0900

    bicmac 0.1.5

--- Casks/bicmac.rb
+++ Casks/bicmac.rb
@@ -1,6 +1,6 @@
 cask "bicmac" do
-  version "0.1.4"
-  sha256 "4bc3acc3e994a9600802cdd86b9cd20bc7d9142f44370c141ba02721a1b90bce"
+  version "0.1.5"
+  sha256 "6ca67a7a19f5b16f0e9e16238ed5fc2cb6c80f72070ca1d2cd5659aaade4ef12"
 
   url "https://github.com/supaicy/BicMac-releases/releases/download/v#{version}/BicMac-#{version}.app.zip"
   name "BicMac"
